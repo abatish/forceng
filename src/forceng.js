57c1f9effe0e6f0f4cd0444e2a51182ed6fd57a2
John Benson <johnedwardbenson@gmail.com>
2016-02-20 16:50:54 -0400

    renamed chatter-service

--- src/forceng.js
+++ src/forceng.js
@@ -5,6 +5,6 @@ require('angular-cache');
 var app = require('angular').module('forceng', ['angular-cache'])
   .factory('force', require('./force'))
   .provider('forceResource', require('./forceng-resource'))
-  .factory('chatterService', require('./chatterService'));
+  .factory('chatterService', require('./chatter-service'));
 
 module.exports = 'forceng';
